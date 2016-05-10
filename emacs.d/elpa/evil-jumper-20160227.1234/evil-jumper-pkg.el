a875295d9060438017f6b7afd04445443cdfc443
Mahendra Gangaiwar <mahendra.gangaiwar@gmail.com>
2016-05-11 01:05:56 +0530

    add

---
+++ emacs.d/elpa/evil-jumper-20160227.1234/evil-jumper-pkg.el
@@ -0,0 +1,1 @@
+(define-package "evil-jumper" "20160227.1234" "Jump like vimmers do!" (quote ((evil "0") (cl-lib "0.5"))))
---
+++ emacs.d/elpa/evil-surround-20160320.1644/evil-surround-pkg.el
@@ -0,0 +1,1 @@
+(define-package "evil-surround" "20160320.1644" "emulate surround.vim from Vim" (quote nil))
